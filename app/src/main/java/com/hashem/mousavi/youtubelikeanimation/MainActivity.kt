e881344c9de66a04e1f22190822e8da2e473a480
hashem <hashem.mousavi.m@gmail.com>
2021-08-21 17:37:26 +0430

    first commit

---
+++ app/src/main/java/com/hashem/mousavi/youtubelikeanimation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hashem.mousavi.youtubelikeanimation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
